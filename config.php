6ccf1759ee924ca04778239c1d182087e314ad0f
willycode <w.illy741026@gmail.com>
2015-04-24 09:41:02 +0000

    Ajax+PHP

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// defines database connection data
+define('DB_HOST', 'willycode-des-1449990');
+define('DB_USER', 'willycode');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ajax'); 
+// defines the number of visible rows in grid
+define('ROWS_PER_VIEW', 10);
+?>
